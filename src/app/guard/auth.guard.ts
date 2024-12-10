f368c2563e5f423182e8150036ce37fd06b98851
yourrlove <nanhvt2708@gmail.com>
2024-12-10 12:23:29 +0700

    feat: admin report action

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngxs/store';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { ApiService } from '../service/api.service';
+import { UserState } from '../store/user/user.state';
 
 export const authGuard = () => {
   const router = inject(Router);
@@ -23,4 +24,15 @@ export const unAuthGuard = () => {
   }
 
   return !token;
+};
+
+export const adminGuard = () => {
+  const store = inject(Store);
+  const userRole = store.selectSnapshot(UserState.currentRole).name;
+
+  if (userRole !== 'admin') {
+    inject(Router).navigate(['/']);
+  }
+
+  return userRole === 'admin';
 };
